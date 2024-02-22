235b97bf6a4dca6ac208f6beefc6d5a2ea0f66c1
Shama Wachanachai <6633053521@student.chula.ac.th>
2024-02-22 21:31:13 +0700

    Add all routes (getOne, getAll, post, put, delete)

--- server.js
+++ server.js
@@ -9,6 +9,38 @@ const app = express();
 app.get('/api/v1/campground', (req, res) => {
     res.status(200).json({
         success: true,
-        msg: 'Show all campground space'
-    })
-})
+        msg: 'Show all campgrounds available'
+    });
+});
+
+app.get('/api/v1/campground/:id', (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: `Show a campground with id: ${req.params.id}`
+    });
+});
+
+app.post('/api/v1/campground', (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: 'Create new campground'
+    });
+});
+
+app.put('/api/v1/campground/:id', (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: `Update campground with id: ${req.params.id}`
+    });
+});
+
+app.delete('/api/v1/campground/:id', (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: `Delete campground with id: ${req.params.id}`
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log('Server is running in ', process.env.NODE_ENV, ' mode on port ', PORT));
