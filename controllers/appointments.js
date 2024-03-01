5c7892d4d3f0123e7c0bb06ec30cf490115c69de
Tippaphanun Kungwarnkanok <tip.kus48@gmail.com>
2024-03-02 01:26:05 +0700

    Add Appointment

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -1,4 +1,5 @@
 const Appointment = require("../models/Appointment");
+const Campground = require("../models/Campground");
 
 // @desc:    Get all appointments
 // @route:   GET api/v1/appointments
@@ -43,5 +44,56 @@ exports.getAppointments = async (req, res, next) => {
       success: false,
       message: "Cannot find Appointment",
     });
+  }
+};
+
+// @desc    Get single appointment
+// @route   GET /api/v1/appointments/:id
+// @access  Public
+exports.getAppointment = async (req, res, next) => {
+  try {
+    const appointment = await Appointment.findById(req.params.id).populate({
+      path: "campground",
+      select: "name province tel",
+    });
+
+    if (!appointment) {
+      return res.status(404).json({
+        success: false,
+        message: `No appointment with the id of ${req.params.id}`,
+      });
+    }
+    res.status(200).json({ success: true, data: appointment });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Cannot find Appointment" });
+  }
+};
+
+// @desc    Add appointment
+// @route   POST /api/v1/campgrounds/:campgroundId/appointments/
+// @access  Private
+exports.addAppointment = async (req, res, next) => {
+  try {
+    req.body.campground = req.params.campgroundId;
+
+    const campground = await Campground.findById(req.params.campgroundId);
+
+    if (!campground) {
+      return res.status(404).json({
+        success: false,
+        message: `No campground with the id of ${req.params.campgroundId}`,
+      });
+    }
+
+    const appointment = await Appointment.create(req.body);
+    res.status(200).json({ success: true, data: appointment });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Cannot create Appointment" });
   }
 };
--- models/Appointment.js
+++ models/Appointment.js
@@ -1,27 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const AppointmentSchema = new mongoose.Schema({
-    appointmentDate: {
-        type: Date,
-        required: true
-    },
+  apptDate: {
+    type: Date,
+    required: true,
+  },
 
-    user: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'User',
-        required: true
-    },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
 
-    campground: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'Campground',
-        required: true
-    },
+  campground: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Campground",
+    required: true,
+  },
 
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model('Appointment', AppointmentSchema);
+module.exports = mongoose.model("Appointment", AppointmentSchema);
