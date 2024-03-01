8fb1261adf73a9de08c1da29a778bb3a78883d49
Shama Wachanachai <6633053521@student.chula.ac.th>
2024-03-01 21:31:55 +0700

    Add advanced filtering, select & sort, and pagination

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -4,11 +4,67 @@ const Campground = require("../models/Campground");
 // @route:   GET /api/v1/campgrounds
 // @access:  Public
 exports.getCampgrounds = async (req, res, next) => {
+  let query;
+
+  const reqQuery = {...req.query};
+  const removeFields = ['select', 'sort', 'page', 'limit'];
+
+  // Loop over remove fields and delete them from reqQuery
+  removeFields.forEach(params => delete reqQuery[params]);
+  console.log(reqQuery);
+
+  let queryStr = JSON.stringify(req.query);
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+  query = Campground.find(JSON.parse(queryStr));
+
+  // Select
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  // Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('name');
+  }
+
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 10;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Campground.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
   try {
-    const campgrounds = await Campground.find();
+    const campgrounds = await query;
+
+    // Pagination result
+    const pagination = {};
+
+    if (endIndex < total) {
+      pagination.next = {
+        page: page + 1,
+        limit
+      }
+    }
+
+    if (startIndex > 0) {
+      pagination.prev = {
+        page: page - 1,
+        limit
+      }
+    }
 
     res.status(200).json({
       success: true,
+      count: campgrounds.length,
+      pagination,
       data: campgrounds,
     });
   } catch (err) {
