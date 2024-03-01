f625423857ab9cf379af5a3227fc1d4333b10b44
Tippaphanun Kungwarnkanok <tip.kus48@gmail.com>
2024-03-01 23:36:50 +0700

    Add Populate Resources

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -1,39 +1,47 @@
-const Appointment = require('../models/Appointment');
+const Appointment = require("../models/Appointment");
 
 // @desc:    Get all appointments
 // @route:   GET api/v1/appointments
 // @access:  Private
 exports.getAppointments = async (req, res, next) => {
-    
-    let query;
+  let query;
 
-    // General users can see only their appointment
-    if (req.user.role !== 'admin') {
-        query = Appointment.find({user: req.user.id});
+  // General users can see only their appointment
+  if (req.user.role !== "admin") {
+    query = Appointment.find({ user: req.user.id }).populate({
+      path: "campground",
+      select: "name province tel",
+    });
+  } else {
+    // If you are an admin, you can see all appointments!
+    if (req.params.campgroundId) {
+      console.log(req.params.campgroundId);
+      query = Appointment.find({
+        campground: req.params.campgroundId,
+      }).populate({
+        path: "campground",
+        select: "name province tel",
+      });
+    } else {
+      query = Appointment.find().populate({
+        path: "campground",
+        select: "name province tel",
+      });
     }
-    else {
-        if (req.params.campgroundId) {
-            console.log(req.params.campgroundId);
-            query = Appointment.find({campground: req.params.campgroundId});
-        }
-        else {
-            query = Appointment.find();
-        }
-    }
+  }
 
-    try {
-        const appointments = await query;
-        res.status(200).json({
-            success: true,
-            count: appointments.length,
-            data: appointments
-        });
-    }
-    catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            success: false,
-            message: "Cannot find Appointment"
-        });
-    }
+  try {
+    const appointments = await query;
+    res.status(200).json({
+      success: true,
+      count: appointments.length,
+      data: appointments,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Cannot find Appointment",
+    });
+  }
 };
--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -6,30 +6,36 @@ const Campground = require("../models/Campground");
 exports.getCampgrounds = async (req, res, next) => {
   let query;
 
-  const reqQuery = {...req.query};
-  const removeFields = ['select', 'sort', 'page', 'limit'];
+  const reqQuery = { ...req.query };
+  const removeFields = ["select", "sort", "page", "limit"];
 
   // Loop over remove fields and delete them from reqQuery
-  removeFields.forEach(params => delete reqQuery[params]);
+  removeFields.forEach((params) => delete reqQuery[params]);
   console.log(reqQuery);
 
   let queryStr = JSON.stringify(req.query);
-  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
 
-  query = Campground.find(JSON.parse(queryStr));
+  // Create operators ($gt, $gte, etc)
+  queryStr = queryStr.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+
+  //Finding resource
+  query = Campground.find(JSON.parse(queryStr)).populate("appointments");
 
   // Select
   if (req.query.select) {
-    const fields = req.query.select.split(',').join(' ');
+    const fields = req.query.select.split(",").join(" ");
     query = query.select(fields);
   }
 
   // Sort
   if (req.query.sort) {
-    const sortBy = req.query.sort.split(',').join(' ');
+    const sortBy = req.query.sort.split(",").join(" ");
     query = query.sort(sortBy);
   } else {
-    query = query.sort('name');
+    query = query.sort("name");
   }
 
   // Pagination
@@ -50,15 +56,15 @@ exports.getCampgrounds = async (req, res, next) => {
     if (endIndex < total) {
       pagination.next = {
         page: page + 1,
-        limit
-      }
+        limit,
+      };
     }
 
     if (startIndex > 0) {
       pagination.prev = {
         page: page - 1,
-        limit
-      }
+        limit,
+      };
     }
 
     res.status(200).json({
