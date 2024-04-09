1efd4dcab37dec37b36f478a634f051c49d89b04
Yiwahpsp <Patsaporn.Kaewkong@gmail.com>
2024-04-09 10:46:25 +0700

    Edit Announcement Schema [adding startDate, endDate and adding message]

--- models/Annoucement.js
+++ models/Annoucement.js
@@ -3,15 +3,25 @@ const mongoose = require('mongoose')
 const AnnouncementSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true
+        required: ['Please add a title ',true]
     },
     content: {
         type: String,
-        required: true
+        required: ['Please add a content',true]
     },
-    date: {
+    startDate: {
         type: Date,
+        required: ['Please add a start date',true],
         default: Date.now
+    },
+    endDate: {
+        type: Date | null,
+        required: ['Please add an end date',true],
+    },
+    campground: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Campground',
+        required: true
     },
     author: {
         type: mongoose.Schema.Types.ObjectId,
