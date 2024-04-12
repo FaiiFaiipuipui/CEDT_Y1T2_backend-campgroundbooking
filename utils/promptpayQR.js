dc328525054958c3776b701983fc66be6f2ea38d
FaiiFaiipuipui <pakarnpahulo2003@gmail.com>
2024-04-12 13:47:58 +0700

    Refactor createPromptPayQR function in promptpayQR.js

--- utils/promptpayQR.js
+++ utils/promptpayQR.js
@@ -1,17 +1,20 @@
 const generatePayload = require('promptpay-qr') 
 const qrcode = require('qrcode') 
-const fs = require('fs') 
 
-const mobileNumber = '000-000-0000' 
-const IDCardNumber = '0-0000-00000-00-0'
-const amount = 100
-const payload = generatePayload(mobileNumber, { amount }) //First parameter : mobileNumber || IDCardNumber
-console.log(payload)
+/**
+ * @param promptPayID is either mobile number or national ID
+ */
+exports.createPromptPayQR = async (promptPayID, amount) => {
+    const payload = generatePayload(promptPayID, { amount });
 
-// Convert to SVG QR Code
-const options = { type: 'svg', color: { dark: '#000', light: '#fff' } }
-qrcode.toString(payload, options, (err, svg) => {
-    if (err) return console.log(err)
-    fs.writeFileSync('./qr.svg', svg)
-    console.log(svg)
-})
+    // Convert to SVG QR Code
+    const options = { type: 'svg', color: { dark: '#000', light: '#fff' } }
+    return qrcode.toString(payload, options, (err, svg) => {
+        if (err) {
+            console.log(err);
+            return 'error';
+        }
+        
+        return svg;
+    })
+}
