61ebe3652a03db1224de9810f59939bf1536274d
FaiiFaiipuipui <pakarnpahulo2003@gmail.com>
2024-04-13 14:28:17 +0700

    Refactor createPromptpayQR function to handle appointmentID and campground price in transactions.js

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,23 +1,51 @@
+const Appointment = require("../models/Appointment");
+const Campground = require("../models/Campground");
 const { createPromptPayQR } = require("../utils/createPromptpayQR")
 
 /**
  * @desc create promptpayQR with req body
  * @route POST /api/v1/transactions
  * @access Private
- * @param req.body : promptpayID, amount
+ * @param req.body : promptpayID, appointmentID
  */
-exports.createPromptpayQR = (req, res, next) => {
+exports.createPromptpayQR = async (req, res, next) => {
   try {
     const data = req.body;
 
-    if (!data.promptpayID || !data.amount) {
-      throw new Error("promptpayID or amount is missing in the request body");
+    if (!data.promptpayID || !data.appointmentID ) {
+      const error = new Error("promptpayID or appointmentID is missing in the request body");
+      error.code = 400;
+      throw error;
     }
 
-    const returnData = createPromptPayQR(data.promptpayID, data.amount);
+    const appointment = await Appointment.findById(data.appointmentID);
+
+    if(!appointment) {
+      const error = new Error(`No appointment with the id of ${req.params.appointmentId}`);
+      error.code = 404;
+      throw error;
+    }
+
+    const campground = await Campground.findById(appointment.campground);
+
+    if (!campground) {
+      const error = new Error(`No campground with the ID of ${appointment.campground}`);
+      error.code = 404;
+      throw error;
+    }
+
+    if (!campground.price) {
+      const error = new Error(`campground with the ID of ${appointment.campground} does not exist price attribute`)
+      error.code = 500;
+      throw error
+    }
+
+    const returnData = createPromptPayQR(data.promptpayID, campground.price);
   
     if (returnData === 'error') {
-      throw new Error("QR code Creation Function failed");
+      const error = new Error("QR code Creation Function failed");
+      error.code = 500;
+      throw error;
     }
 
     res.status(200).json({
@@ -26,9 +54,9 @@ exports.createPromptpayQR = (req, res, next) => {
     })
   } catch (err) {
     console.log(err);
-    res.status(500).json({
+    res.status(err.code || 500).json({
       success: false,
-      message: "Failed to create promptpayQR",
+      message: err.message,
     });
   }
 }
