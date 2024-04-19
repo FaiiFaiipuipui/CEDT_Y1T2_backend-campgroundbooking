3a01284071a28bb17bbc978629a4967391fefc6c
FaiiFaiipuipui <pakarnpahulo2003@gmail.com>
2024-04-19 10:04:59 +0700

    Update limit to 100 in getTransactions controller

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -137,7 +137,7 @@ exports.getTransactions = async (req, res, next) => {
 
   // Pagination
   const page = parseInt(req.query.page, 10) || 1;
-  const limit = parseInt(req.query.limit, 10) || 10;
+  const limit = parseInt(req.query.limit, 10) || 100;
   const startIndex = (page - 1) * limit;
   const endIndex = page * limit;
   const total = await Transaction.countDocuments();
